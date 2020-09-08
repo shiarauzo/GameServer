8434c60710f48d467d3e51f58731a8886e58a1e6
u201614342@upc.edu.pe <u201614342@upc.edu.pe>
2020-09-08 11:51:16 -0500

    game server y cliente servidor junto con una base para juego en unity

---
+++ GameServer/GameServer/GameLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace GameServer
+{
+    class GameLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
